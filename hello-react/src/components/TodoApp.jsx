68cc4f5eaecd2a04474e76c1ca84dd134de5ced9
Sadık Turan <sadikturangmail.com>
2024-11-08 10:44:18 +0300

    8-todo-app-2

--- hello-react/src/components/TodoApp.jsx
+++ hello-react/src/components/TodoApp.jsx
@@ -9,28 +9,53 @@ const products = [
 ];
 
 export default function TodoApp() {
-  return (
-    <div className="container">
-      <Header />
-      <Form />
-      <List />
-      <Summary />
-    </div>
-  );
-}
-
-function Header() {
-  return <h1>TodoApp</h1>;
-}
-
-function Form() {
-  const [itemName, setItemName] = useState("");
-  const [quantity, setQuantity] = useState(1);
   const [items, setItems] = useState(products);
 
   function handleAddItem(item) {
     setItems((prevItems) => [...prevItems, item]);
   }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleUpdateItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  }
+
+  return (
+    <div className="container my-3">
+      <div className="card">
+        <Header />
+        <div className="card-body">
+          <Form onAddItem={handleAddItem} />
+          <List
+            items={items}
+            onDeleteItem={handleDeleteItem}
+            onUpdateItem={handleUpdateItem}
+          />
+        </div>
+        <Summary items={items} />
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="card-header">
+      <h1 className="h3">TodoApp</h1>
+    </div>
+  );
+}
+
+function Form({ onAddItem }) {
+  const [itemName, setItemName] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleFormSubmit(e) {
     e.preventDefault();
@@ -44,7 +69,7 @@ function Form() {
 
     console.log(product);
 
-    handleAddItem(product);
+    onAddItem(product);
 
     setItemName("");
     setQuantity(1);
@@ -52,46 +77,100 @@ function Form() {
 
   return (
     <form onSubmit={handleFormSubmit}>
-      <input
-        type="text"
-        placeholder="Eleman ekle"
-        name="itemName"
-        value={itemName}
-        onChange={(e) => setItemName(e.target.value)}
-      />
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
-        {Array.from({ length: 10 }, (v, i) => i + 1).map((num) => (
-          <option key={num} value={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <button type="submit">Ekle</button>
+      <div className="row">
+        <div className="col-10">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Eleman ekle"
+            name="itemName"
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}
+          />
+        </div>
+        <div className="col">
+          <select
+            className="form-select"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          >
+            {Array.from({ length: 10 }, (v, i) => i + 1).map((num) => (
+              <option key={num} value={num}>
+                {num}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="col">
+          <button className="btn btn-primary" type="submit">
+            Ekle
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
 
-function List() {
+function List({ items, onDeleteItem, onUpdateItem }) {
   return (
-    <ul>
-      {products.map((p, index) => (
-        <ListItem item={p} key={index} />
-      ))}
-    </ul>
+    <>
+      {items.length > 0 ? (
+        <ul className="list-group mt-3">
+          {items.map((p, index) => (
+            <ListItem
+              item={p}
+              key={index}
+              onDeleteItem={onDeleteItem}
+              onUpdateItem={onUpdateItem}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p className="text-danger">Eleman yok</p>
+      )}
+    </>
   );
 }
 
-function ListItem({ item }) {
+function ListItem({ item, onDeleteItem, onUpdateItem }) {
   return (
-    <li>
-      <span style={item.completed ? { textDecoration: "line-through" } : {}}>
-        {item.title} - {item.quantity}
-      </span>
-      <button>X</button>
+    <li className="list-group-item d-flex justify-content-between">
+      <div>
+        <input
+          type="checkbox"
+          className="form-check-input me-2"
+          checked={item.completed}
+          onChange={() => onUpdateItem(item.id)}
+        />
+        <span style={item.completed ? { textDecoration: "line-through" } : {}}>
+          {item.title} - {item.quantity}
+        </span>
+      </div>
+
+      <button
+        className="btn btn-sm btn-danger"
+        onClick={() => onDeleteItem(item.id)}
+      >
+        X
+      </button>
     </li>
   );
 }
 
-function Summary() {
-  return <div>Alışveriş sepetinizde 10 ürün bulunmaktadır.</div>;
+function Summary({ items }) {
+  const itemsCount = items.length;
+  const completedItemsCount = items.filter((i) => i.completed).length;
+
+  return (
+    <footer className="card-footer">
+      {itemsCount === completedItemsCount ? (
+        <p className="mb-0">😊 Alışveriş tamamlandı.</p>
+      ) : (
+        <p className="mb-0">
+          Alışveriş sepetinizde {itemsCount} üründen {completedItemsCount}{" "}
+          tanesini aldınız.
+        </p>
+      )}
+    </footer>
+  );
 }
